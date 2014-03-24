1f644689507ec54c2fe611f72ec41d90071221a2
Vincent Durand <vincent.durand@madwork.org>
2014-03-24 16:08:36 +0100

    Update description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'chruby-build'
 maintainer       'Vincent Durand'
 maintainer_email 'vincent.durand@madwork.org'
 license          'MIT'
-description      'Installs/Configures chruby'
+description      'Chef cookbook to install chruby and build rubies from source with Google Perftools and LibYAML options.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'ubuntu', '>= 12.04'
