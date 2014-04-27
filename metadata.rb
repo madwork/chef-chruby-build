4cbf701118bfe6d4729944cdb0dce0b779ee9364
Vincent Durand <vincent.durand@madwork.org>
2014-04-27 12:00:01 +0200

    Bump to v0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vincent.durand@madwork.org'
 license          'MIT'
 description      'Chef cookbook to install chruby and build rubies from source with Google Perftools and LibYAML options.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 supports         'ubuntu', '>= 12.04'
 depends          'apt', '>= 2.3'
 depends          'ark', '>= 0.6'
