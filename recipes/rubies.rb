58f86410a4370bd796f84cc2e4fb54fdfc24238d
Vincent Durand <vincent.durand@madwork.org>
2014-08-11 14:44:11 +0200

    Add missing ostruct require

--- recipes/rubies.rb
+++ recipes/rubies.rb
@@ -23,6 +23,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+require 'ostruct'
 
 include_recipe "chruby-build::default"
 
